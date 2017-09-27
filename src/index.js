0666518317cb3e6a1a5247cf4ba538a158049520
rr_alves <rr.alves@campus.fct.unl.pt>
2017-09-27 19:35:38 +0100

    With the number of images waits 500ms for each to load.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const { MarkdownToHTML } = require('./MarkdownToHTML');
-const phantom = require('phantom');
+const phantom = require('phantom'); // https://github.com/amir20/phantomjs-node
 
 class CLI {
 
@@ -22,15 +22,29 @@ class CLI {
         const page = await phantomInstance.createPage();
         await page.property('viewportSize', { width: 1024, height: 600 });
         page.setContent(htmlCode, '');
-        
-    /*     await page.render('stackoverflow.pdf')
-        console.log(`File created at [./stackoverflow.html]`);
-        await phantomInstance.exit(); */
-        
-        setTimeout(() => {
-            page.render('stackoverflow.pdf')
-            phantomInstance.exit();
-        }, 3000);
+
+        //Verify if htmlCode contains images
+
+        /*     await page.render('stackoverflow.pdf')
+            console.log(`File created at [./stackoverflow.html]`);
+            await phantomInstance.exit(); */
+        let numImgs = this._numImagesInHtmlCode(htmlCode);
+        if (numImgs === 0) {
+            console.log("0 images");
+            await page.render('stackoverflow.pdf');
+            await phantomInstance.exit();
+        } else {
+            console.log(`Has ${numImgs} images.`)
+            setTimeout(() => {
+                page.render('stackoverflow.pdf').then((page) => {
+                    phantomInstance.exit();
+                }).catch(e => console.log(e));
+            }, 500 * numImgs);
+        }
+    }
+
+    _numImagesInHtmlCode(htmlCode) {
+        return (htmlCode.match(/<img/g) || []).length;
     }
 }
 let markdownCode = fs.readFileSync('D:\\BoostNote Folder\\dist\\Test 1\\Boostnote is amazing.md', 'utf8');
